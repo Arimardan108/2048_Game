b5e286f44c2c85405c84522ae04554129d0af427
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-13 13:02:39 +0530

    Update file

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const gridDisplay = document.querySelector(".grid");
-
   const scoreDisplay = document.querySelector("#score");
 
   const resultDisplay = document.querySelector("#result");
