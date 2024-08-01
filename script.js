39daffbc80cbe22e26c21c959d3853c3b1816f8b
Arimardan108 <mishraarimardan934@gmail.com>
2024-08-01 18:38:40 +0530

    Delete file

--- script.js
+++
@@ -1,6 +0,0 @@
-import grid from "./grid";
-import tile from "./tile";
-const BordGame = document.getElementsByClassName("bord-game");
-const grid = new grid(BordGame);
-grid.randommptyCell().tile = new tile(BordGame);
-grid.randommptyCell().tile = new tile(BordGame);
