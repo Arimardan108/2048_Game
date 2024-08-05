740ab0863bfda1160bebf5beeaf9222fe7919f7c
Arimardan108 <mishraarimardan934@gmail.com>
2024-08-05 20:43:52 +0530

    update file

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ import Grid from "./Grid ";
 import Tile from "./tile";
 const Boardgame = document.getElementById("board-game");
 const grid = new Boardgame();
+grid.randomEmptyCell().tile = new Tile(Boardgame);
+grid.randomEmptyCell().tile = new Tile(Boardgame);
