c5914b97047891ab9e1b1ac788784d2f40092b01
Arimardan108 <mishraarimardan934@gmail.com>
2024-08-05 21:00:55 +0530

    update file

---
+++ tile.js
@@ -0,0 +1,16 @@
+export default class Tile {
+  #tileElement;
+  #x;
+  #y;
+  #value;
+  constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
+    this.#tileElement = document.createElement("div");
+    this.#tileElement = classList.add("tile");
+    tileContainer.append(this.#tileElement);
+    this.value = value;
+  }
+  set x(value) {
+    this.#x = value;
+    this.#tileElement.style.setProperty("--x", value);
+  }
+}
