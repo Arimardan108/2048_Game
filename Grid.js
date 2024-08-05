14c9d4906efb537a76190afbc9c25e4fb0e86cc7
Arimardan108 <mishraarimardan934@gmail.com>
2024-08-05 11:44:13 +0530

    update file

--- Grid.js
+++ Grid.js
@@ -5,9 +5,32 @@ export default class Grid {
   constructor(gridElements) {
     gridElements.style.setProperty("--grid_size", GRID_SIZE);
     gridElements.style.setProperty("--cell_size", `${CELL_SIZE}vmin`);
-    gridElements.style.setProperty("--cell_gap"), `${CELL_SIZE}vmin`;
+    gridElements.style.setProperty("--cell_gap"), `${CELL_GAP}vmin`;
+    this.cells = createCellElement(gridElement).map((cellElement, index) => {
+      return new Cell(
+        cellElement,
+        index % GRID_SIZE,
+        Math.floor(index / GRID_SIZE)
+      );
+    });
   }
 }
+
+class Cell {
+  constructor(cellElement, x, y) {
+    this.cellElement = cellElement;
+    this.x = x;
+    this.y = y;
+  }
+}
+
 function createCellElement(gridElements) {
   const cells = [];
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cells.push(cell);
+    gridElements.append(cell);
+  }
+  return cells;
 }
