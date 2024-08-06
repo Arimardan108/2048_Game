e4ecfe873555d5ef7ee6f6049c0ada23c3136f1b
Arimardan108 <mishraarimardan934@gmail.com>
2024-08-06 07:05:28 +0530

    update file

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-import Grid from "./Grid ";
-import Tile from "./tile";
+import Grid from "./Grid.js ";
+import Tile from "./tile.js";
 const Boardgame = document.querySelector(".board-game");
 const grid = new Grid(Boardgame);
 grid.randomEmptyCell().tile = new Tile(Boardgame);
 grid.randomEmptyCell().tile = new Tile(Boardgame);
+
+setupInput();
+function setupInput() {
+  window.addEventListener("keydown", handleInput, { once: true });
+}
+function handleInput(e) {
+  switch (e.key) {
+    case "ArrowUp":
+      moveUp();
+      break;
+    case "ArrowDown":
+      moveDown();
+  }
+}
